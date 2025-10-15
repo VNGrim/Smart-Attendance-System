620e2b6cde4d857b28e76ed7de79e45780e9ffc4
Trung Lê <trunghuynh.220803@gmail.com>
2025-10-16 06:10:46 +0700

    feat: add trang Lớp học

---
+++ frontend/src/app/lop_gv/layout.tsx
@@ -0,0 +1,22 @@
+import "./global.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Lịch học sinh viên",
+  description: "Trang lịch học của hệ thống điểm danh",
+};
+
+export default function LichHocLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="vi">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
