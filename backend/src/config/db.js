1c1537b6aac1be3fed09495aeeba1b46a4ce9391
Văn đẹp trai Đỗ <dodinhvan01@gmail.com>
2025-10-26 03:14:14 +0700

    Dashboard cua admin

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,7 @@ dotenv.config({ path: path.join(__dirname, '../../db.env') });
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "0862464894",
+  password: process.env.DB_PASSWORD || "111111",
   database: process.env.DB_NAME || "qlsv",
 });
 
