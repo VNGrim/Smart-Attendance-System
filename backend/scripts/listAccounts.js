26b9ec1f485b3eddcc1b6775ca6e1d0e55c5aaba
Trung Lê <trunghuynh.220803@gmail.com>
2025-11-04 10:52:24 +0700

    chore: Remove temporary test scripts

--- backend/scripts/listAccounts.js
+++
@@ -1,9 +0,0 @@
-(async ()=>{
-  try{
-    const prisma=require('../src/config/prisma');
-    await prisma.$connect();
-    const acc=await prisma.accounts.findMany({take:50});
-    console.log(JSON.stringify(acc,null,2));
-    process.exit(0);
-  }catch(e){console.error(e); process.exit(1);} 
-})();
